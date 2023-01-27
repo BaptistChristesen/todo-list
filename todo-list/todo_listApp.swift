72acdeebc0ba8267542209df769175aa1efb057d
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-01-27 08:20:09 -0600

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by caden christesen on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
