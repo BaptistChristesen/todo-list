94cfc170b3452dadedb37cec5dc48afc8b1afb52
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-01-27 08:55:53 -0600

    added due dates and priority markers to the todo list

---
+++ todo-list/todoList.swift
@@ -0,0 +1,14 @@
+//
+//  todoList.swift
+//  todo-list
+//
+//  Created by caden christesen on 1/27/23.
+//
+
+import Foundation
+
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
